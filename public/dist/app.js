a7401e0b2a15af3c77cb72ad2542bfc33bb4a982
Mike Rockall <mrockall@gmail.com>
2015-04-19 15:44:29 +0100

    Render the feelings options on the login page.

--- public/dist/app.js
+++ public/dist/app.js
@@ -22703,7 +22703,7 @@ Velocity's structure:
     }
     
     this.masterPages = [];
-    
+
     div = document.createElement('div');
     div.id = 'swipeview-slider';
     div.style.cssText = 'position:relative;top:0;height:100%;width:100%;' + cssVendor + 'transition-duration:0;' + cssVendor + 'transform:translateZ(0);' + cssVendor + 'transition-timing-function:ease-out';
@@ -23037,6 +23037,7 @@ Velocity's structure:
     var jade=function(){function e(e){return null!=e&&""!==e}function n(t){return(Array.isArray(t)?t.map(n):t&&"object"==typeof t?Object.keys(t).filter(function(e){return t[e]}):[t]).filter(e).join(" ")}var t={};return t.merge=function r(n,t){if(1===arguments.length){for(var a=n[0],i=1;i<n.length;i++)a=r(a,n[i]);return a}var o=n["class"],s=t["class"];(o||s)&&(o=o||[],s=s||[],Array.isArray(o)||(o=[o]),Array.isArray(s)||(s=[s]),n["class"]=o.concat(s).filter(e));for(var l in t)"class"!=l&&(n[l]=t[l]);return n},t.joinClasses=n,t.cls=function(e,r){for(var a=[],i=0;i<e.length;i++)a.push(r&&r[i]?t.escape(n([e[i]])):n(e[i]));var o=n(a);return o.length?' class="'+o+'"':""},t.style=function(e){return e&&"object"==typeof e?Object.keys(e).map(function(n){return n+":"+e[n]}).join(";"):e},t.attr=function(e,n,r,a){return"style"===e&&(n=t.style(n)),"boolean"==typeof n||null==n?n?" "+(a?e:e+'="'+e+'"'):"":0==e.indexOf("data")&&"string"!=typeof n?(-1!==JSON.stringify(n).indexOf("&")&&console.warn("Since Jade 2.0.0, ampersands (`&`) in data attributes will be escaped to `&amp;`"),n&&"function"==typeof n.toISOString&&console.warn("Jade will eliminate the double quotes around dates in ISO form after 2.0.0")," "+e+"='"+JSON.stringify(n).replace(/'/g,"&apos;")+"'"):r?(n&&"function"==typeof n.toISOString&&console.warn("Jade will stringify dates in ISO form after 2.0.0")," "+e+'="'+t.escape(n)+'"'):(n&&"function"==typeof n.toISOString&&console.warn("Jade will stringify dates in ISO form after 2.0.0")," "+e+'="'+n+'"')},t.attrs=function(e,r){var a=[],i=Object.keys(e);if(i.length)for(var o=0;o<i.length;++o){var s=i[o],l=e[s];"class"==s?(l=n(l))&&a.push(" "+s+'="'+l+'"'):a.push(t.attr(s,l,!1,r))}return a.join("")},t.escape=function(e){var n=String(e).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;");return n===""+e?e:n},t.rethrow=function a(e,n,t,r){if(!(e instanceof Error))throw e;if(!("undefined"==typeof window&&n||r))throw e.message+=" on line "+t,e;try{r=r||require("fs").readFileSync(n,"utf8")}catch(i){a(e,null,t)}var o=3,s=r.split("\n"),l=Math.max(t-o,0),f=Math.min(s.length,t+o),o=s.slice(l,f).map(function(e,n){var r=n+l+1;return(r==t?"  > ":"    ")+r+"| "+e}).join("\n");throw e.path=n,e.message=(n||"Jade")+":"+t+"\n"+o+"\n\n"+e.message,e},t}();
 
     var templatizer = {};
+    templatizer["login"] = {};
     templatizer["modals"] = {};
     templatizer["my_round"] = {};
     templatizer["tournaments"] = {};
@@ -23044,6 +23045,18 @@ Velocity's structure:
     // body.jade compiled template
     templatizer["body"] = function tmpl_body() {
         return '<div class="main"><div class="content"><header><div href="/" class="logo"></div><div class="title">Blue Pirate #7</div><div class="subtitle">Athenry Golf Course</div><div class="tabs"><a href="/leaderboard" class="ldrboard">Leaderboard</a><a href="/my-round" class="me_user">Me</a></div></header><div class="container pages page-container"><div class="page"></div></div></div><div role="modal-container" class="modals"><div class="modal"></div></div></div>';
+    };
+
+    // login/option.jade compiled template
+    templatizer["login"]["option"] = function tmpl_login_option(locals) {
+        var buf = [];
+        var jade_mixins = {};
+        var jade_interp;
+        var locals_for_with = locals || {};
+        (function(label) {
+            buf.push('<div class="option">' + jade.escape(null == (jade_interp = label) ? "" : jade_interp) + "</div>");
+        }).call(this, "label" in locals_for_with ? locals_for_with.label : typeof label !== "undefined" ? label : undefined);
+        return buf.join("");
     };
 
     // modals/nine_box.jade compiled template
@@ -23108,7 +23121,7 @@ Velocity's structure:
 
     // my_round/login.jade compiled template
     templatizer["my_round"]["login"] = function tmpl_my_round_login() {
-        return '<div class="login_page"><p>Enter your unique passphrase to login and begin keeping the score.</p><div class="options"><div class="row"><div class="option">Angry</div><div class="option">Delighted</div></div><div class="row"><div class="option">Excited</div><div class="option">Scared</div></div><div class="row"><div class="option">Motivated</div><div class="option">Surprised</div></div><div class="row"><div class="option">Confused</div><div class="option">Happy</div></div></div></div>';
+        return '<div class="login_page"><p>Enter your unique passphrase to login and begin keeping score.</p><div class="options"></div></div>';
     };
 
     // my_round/view.jade compiled template
@@ -23973,17 +23986,63 @@ var Model = require('ampersand-model');
 var templates = require('../../../dist/templates');
 
 var LoginModel = Model.extend({
+  props: {
+    feeling: '',
+    colour: '',
+    animal: '',
 
+    feeling_options: {
+      type: 'array'
+    }
+  },
+
+  initialize: function(){
+    this.feeling_options = [
+      'Angry', 'Delighted', 'Excited', 'Scared', 'Motivated',
+      'Surprised', 'Confused', 'Happy', 'Anxious', 'Encouraged'
+    ]
+  }
 });
+
+var OptionsView = View.extend({
+  template: templates.login.option,
+  props: {
+    label: ''
+  },
+  events: {
+    'click .option': 'selectOption'
+  },
+  selectOption: function(ev){
+    $(ev.delegateTarget).one(app.whichTransitionEvent, function() {
+      $(this).removeClass('ripple');
+    }).addClass('ripple');
+  }
+})
 
 module.exports = View.extend({
   template: templates.my_round.login,
 
+  initialize: function(){
+    this.model = new LoginModel();
+  },
+
   render: function(){
-    console.trace();
-    return this.renderWithTemplate();
+    this.renderWithTemplate();
+
+    this.renderFeelingOptions();
+  },
+
+  renderFeelingOptions: function(){
+    _(this.model.feeling_options).each(_.bind(function(item, i){
+      var option_view = new OptionsView({
+        label: item
+      });
+
+      this.renderSubview(option_view, '.options');
+    }, this));
   }
 });
+
 },{"../../../dist/templates":"/Library/WebServer/Server/bp_tournaments/public/dist/templates.js","ampersand-model":"/Library/WebServer/Server/bp_tournaments/node_modules/ampersand-model/ampersand-model.js","ampersand-view":"/Library/WebServer/Server/bp_tournaments/node_modules/ampersand-view/ampersand-view.js","jquery":"/Library/WebServer/Server/bp_tournaments/node_modules/jquery/dist/jquery.js","underscore":"/Library/WebServer/Server/bp_tournaments/node_modules/underscore/underscore.js"}],"/Library/WebServer/Server/bp_tournaments/public/js/views/my_round/view.js":[function(require,module,exports){
 // ---- Vendor ----
 var _ = require('underscore');
--- public/js/views/my_round/login.js
+++ public/js/views/my_round/login.js
@@ -8,14 +8,59 @@ var Model = require('ampersand-model');
 var templates = require('../../../dist/templates');
 
 var LoginModel = Model.extend({
+  props: {
+    feeling: '',
+    colour: '',
+    animal: '',
 
+    feeling_options: {
+      type: 'array'
+    }
+  },
+
+  initialize: function(){
+    this.feeling_options = [
+      'Angry', 'Delighted', 'Excited', 'Scared', 'Motivated',
+      'Surprised', 'Confused', 'Happy', 'Anxious', 'Encouraged'
+    ]
+  }
 });
+
+var OptionsView = View.extend({
+  template: templates.login.option,
+  props: {
+    label: ''
+  },
+  events: {
+    'click .option': 'selectOption'
+  },
+  selectOption: function(ev){
+    $(ev.delegateTarget).one(app.whichTransitionEvent, function() {
+      $(this).removeClass('ripple');
+    }).addClass('ripple');
+  }
+})
 
 module.exports = View.extend({
   template: templates.my_round.login,
 
+  initialize: function(){
+    this.model = new LoginModel();
+  },
+
   render: function(){
-    console.trace();
-    return this.renderWithTemplate();
+    this.renderWithTemplate();
+
+    this.renderFeelingOptions();
+  },
+
+  renderFeelingOptions: function(){
+    _(this.model.feeling_options).each(_.bind(function(item, i){
+      var option_view = new OptionsView({
+        label: item
+      });
+
+      this.renderSubview(option_view, '.options');
+    }, this));
   }
 });
