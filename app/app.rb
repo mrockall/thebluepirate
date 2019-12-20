12d62bbc84d61d4df721f4537eb13ee6a569c0b5
Mike Rockall <mrockall@gmail.com>
2019-12-20 12:41:35 +0000

    Changed the title of the app.

--- app/app.rb
+++ app/app.rb
@@ -64,7 +64,7 @@ module BluePirate
       @courses = Rabl.render(c, 'courses/view', :view_path => 'app/views', :format => 'json')
       @holes = Rabl.render(c.holes, 'holes/view', :view_path => 'app/views', :format => 'json')
       
-      @title = "Blue Pirate"
+      @title = "Liam & Lisa Open"
       render :index
     end
   end
